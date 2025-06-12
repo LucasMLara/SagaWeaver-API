42bfcbb7294690c80f722bbc66b65217b5064a44
Lucas M. Lara <LucasML@Live.com>
2025-06-12 20:23:11 -0300

    API EntryPoint

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './graphql/schema';
+import { resolvers } from './graphql/resolvers';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+}
+
+startServer();
